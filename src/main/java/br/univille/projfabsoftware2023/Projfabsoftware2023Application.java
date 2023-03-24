07e851871f484a80e706d2e0c1cae82c53aab93d
MATHEUS AUGUSTO ZEFERINO <matheus.zeferino@univille.br>
2023-03-24 20:27:34 -0300

    First commit

---
+++ src/main/java/br/univille/projfabsoftware2023/Projfabsoftware2023Application.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftware2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projfabsoftware2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projfabsoftware2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/projfabsoftware2023/Projfabsoftware2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftware2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projfabsoftware2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
